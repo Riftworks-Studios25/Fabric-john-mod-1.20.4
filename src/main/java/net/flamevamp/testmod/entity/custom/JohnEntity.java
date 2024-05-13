c89c7e31cbebf9e6d54b2d4cb92aca75ea51fb6c
BuildTools <unconfigured@null.spigotmc.org>
2024-05-12 19:50:40 -0700

    fixed witch nose and JOHN is now immune to projectiles while under 1/2 health

--- src/main/java/net/flamevamp/testmod/entity/custom/JohnEntity.java
+++ src/main/java/net/flamevamp/testmod/entity/custom/JohnEntity.java
@@ -1,23 +1,27 @@
 package net.flamevamp.testmod.entity.custom;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityDimensions;
-import net.minecraft.entity.EntityPose;
-import net.minecraft.entity.EntityType;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.entity.*;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.boss.BossBar;
 import net.minecraft.entity.boss.ServerBossBar;
+import net.minecraft.entity.boss.WitherEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.registry.tag.DamageTypeTags;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -40,6 +44,21 @@ public class JohnEntity extends HostileEntity {
         this.bossBar.addPlayer(player);
     }
 
+    public boolean shouldRenderOverlay() {
+        return this.getHealth() <= this.getMaxHealth() / 2.0f;
+    }
+
+    @Override
+    public boolean damage(DamageSource source, float amount) {
+        Entity entity;
+        if (this.isInvulnerableTo(source)) {
+            return false;
+        }
+        if (this.shouldRenderOverlay() && (entity = source.getSource()) instanceof PersistentProjectileEntity) {
+            return false;
+        }
+        return super.damage(source, amount);
+    }
     @Override
     protected void mobTick() {
         this.bossBar.setPercent(this.getHealth() / this.getMaxHealth());
